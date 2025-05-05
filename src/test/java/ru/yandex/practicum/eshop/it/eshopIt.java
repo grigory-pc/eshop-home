309388c803621f6ea13f0a2c41ba0edb11e69cd9
Grigory <grigoriy.trubnikov@ebs.ru>
2025-05-05 17:12:11 +0300

    feat: заготовка для тестов

---
+++ src/test/java/ru/yandex/practicum/eshop/it/eshopIt.java
@@ -0,0 +1,56 @@
+package ru.yandex.practicum.eshop.it;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class eshopIt {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void redirectToMainItems() {
+  }
+
+  @Test
+  void getItems() {
+  }
+
+  @Test
+  void updateMainCartItems() {
+  }
+
+  @Test
+  void getCartItems() {
+  }
+
+  @Test
+  void updateCartItems() {
+  }
+
+  @Test
+  void getItemById() {
+  }
+
+  @Test
+  void updateItems() {
+  }
+
+  @Test
+  void buyItems() {
+  }
+
+  @Test
+  void getOrders() {
+  }
+
+  @Test
+  void getOrderById() {
+  }
+}
---
+++ src/test/java/ru/yandex/practicum/eshop/service/ItemServiceImplTest.java
@@ -0,0 +1,41 @@
+package ru.yandex.practicum.eshop.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemServiceImplTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void getItems() {
+  }
+
+  @Test
+  void editCart() {
+  }
+
+  @Test
+  void getCartItems() {
+  }
+
+  @Test
+  void getItem() {
+  }
+
+  @Test
+  void buyItems() {
+  }
+
+  @Test
+  void getOrders() {
+  }
+
+  @Test
+  void getOrderItems() {
+  }
+}
