5c3decb58c4b6dfbaf0e446a753bf6431f0391a8
Grigory <grigoriy.trubnikov@ebs.ru>
2025-05-05 16:30:56 +0300

    feat: рефакторинг ItemService

--- src/main/java/ru/yandex/practicum/eshop/service/ItemServiceImpl.java
+++ src/main/java/ru/yandex/practicum/eshop/service/ItemServiceImpl.java
@@ -35,7 +35,6 @@ import ru.yandex.practicum.eshop.repository.OrderRepository;
 public class ItemServiceImpl implements ItemService {
   private static final Long CART_ID = 1L;
   private static final Double TOTAL_INIT = 0.00;
-  private static final Integer COUNT_INIT = 0;
   private final ItemRepository itemRepository;
   private final ItemMapper itemMapper;
   private final CartRepository cartRepository;
@@ -46,16 +45,8 @@ public class ItemServiceImpl implements ItemService {
   @Override
   public Page<ItemDto> getItems(String search, Sorting sort, int pageNumber, int pageSize) {
     Page<Item> items;
-    Pageable pageableItems;
 
-    switch (sort) {
-      case ALPHA ->
-          pageableItems = PageRequest.of(pageNumber, pageSize, Sort.by("title").ascending());
-      case PRICE ->
-          pageableItems = PageRequest.of(pageNumber, pageSize, Sort.by("price").ascending());
-      case NO -> pageableItems = PageRequest.of(pageNumber, pageSize);
-      default -> throw new SortingException("Некорректный тип сортировки: " + sort);
-    }
+    Pageable pageableItems = getPageableItemsRequest(sort, pageNumber, pageSize);
 
     if (search.isEmpty()) {
       items = itemRepository.findAll(pageableItems);
@@ -75,62 +66,18 @@ public class ItemServiceImpl implements ItemService {
                                                                                            itemId);
 
     switch (action) {
-      case PLUS -> {
-        if (existingCartItem.isPresent()) {
-          CartItem cartItem = existingCartItem.get();
-
-          cartItem.setCount(cartItem.getCount() + 1);
-          cartItemRepository.save(cartItem);
-        } else {
-          CartItem newItem = new CartItem();
-          newItem.setCartId(CART_ID);
-          newItem.setItemId(itemId);
-          newItem.setCount(1);
-
-          cartItemRepository.save(newItem);
-        }
-        itemRepository.incrementCount(itemId);
-      }
-      case MINUS -> {
-        if (existingCartItem.isPresent()) {
-          CartItem cartItem = existingCartItem.get();
-
-          if (cartItem.getCount() >= 1) {
-            cartItem.setCount(cartItem.getCount() - 1);
-            cartItemRepository.save(cartItem);
-
-            itemRepository.decrementCount(itemId);
-          } else {
-            cartItemRepository.delete(cartItem);
-          }
-        }
-      }
+      case PLUS -> incrementItem(itemId, existingCartItem);
+      case MINUS -> decrementItem(itemId, existingCartItem);
       case DELETE -> existingCartItem.ifPresent(cartItemRepository::delete);
-
     }
 
-    double total = cartItemRepository.findCartItemsByCartId(CART_ID)
-                                     .stream()
-                                     .mapToDouble(cartItem -> {
-                                       Item product = itemRepository.getReferenceById(
-                                           cartItem.getItemId());
-                                       return product.getPrice() * cartItem.getCount();
-                                     })
-                                     .sum();
-
-    existingCart.setTotal(total);
+    existingCart.setTotal(calculateTotal());
     cartRepository.save(existingCart);
   }
 
   @Override
   public CartDto getCartItems() {
-    List<Item> items = cartItemRepository.findCartItemsByCartId(CART_ID)
-                                         .stream()
-                                         .map(CartItem::getItemId)
-                                         .collect(Collectors.collectingAndThen(
-                                             Collectors.toList(),
-                                             itemRepository::findAllById
-                                         ));
+    List<Item> items = getItemsFromCart();
 
     double total = items.stream()
                         .mapToDouble(item -> item.getPrice() * item.getCount())
@@ -153,23 +100,14 @@ public class ItemServiceImpl implements ItemService {
     Cart cart = cartRepository.getReferenceById(CART_ID);
     List<Item> items = new ArrayList<>(cart.getItems());
 
-
-    Order order = new Order();
-    order.setTotalSum(cart.getTotal());
-    order.setItems(items);
+    Order order = Order.builder()
+                       .items(items)
+                       .totalSum(cart.getTotal())
+                       .build();
 
     Order savedOrder = orderRepository.save(order);
 
-    List<OrderItem> orderItems = items.stream()
-                                      .map(item -> {
-                                        int count = item.getCount() != null ? item.getCount() : 0;
-                                        return new OrderItem(savedOrder.getId(), item.getId(),
-                                                             count);
-                                      })
-                                      .toList();
-
-    orderItemRepository.saveAll(orderItems);
-
+    orderItemRepository.saveAll(createAndGetOrderItems(items, savedOrder));
 
     flushItemAndCart(cart.getId());
 
@@ -187,22 +125,9 @@ public class ItemServiceImpl implements ItemService {
 
   @Override
   public OrderDto getOrderItems(Long id) {
-    Map<Long, Integer> orderItemCounts = orderItemRepository.findOrderItemsByOrderId(id)
-                                                            .stream()
-                                                            .collect(Collectors.toMap(
-                                                                OrderItem::getItemId,
-                                                                OrderItem::getCount
-                                                            ));
+    Map<Long, Integer> orderItemCounts = getItemsCountFromOrderItems(id);
 
-    List<Item> items = itemRepository.findAllById(orderItemCounts.keySet())
-                                     .stream()
-                                     .peek(item -> {
-                                       Integer countFromOrder = orderItemCounts.get(item.getId());
-                                       if (countFromOrder != null) {
-                                         item.setCount(countFromOrder);
-                                       }
-                                     })
-                                     .toList();
+    List<Item> items = getItemsFromOrder(orderItemCounts);
 
     double total = items.stream()
                         .mapToDouble(item -> item.getPrice() * item.getCount())
@@ -215,6 +140,37 @@ public class ItemServiceImpl implements ItemService {
                    .build();
   }
 
+  private static List<OrderItem> createAndGetOrderItems(List<Item> items, Order savedOrder) {
+    return items.stream()
+                .map(item -> {
+                  int count = item.getCount() != null ? item.getCount() : 0;
+                  return new OrderItem(savedOrder.getId(), item.getId(),
+                                       count);
+                })
+                .toList();
+  }
+
+  private List<Item> getItemsFromOrder(Map<Long, Integer> orderItemCounts) {
+    return itemRepository.findAllById(orderItemCounts.keySet())
+                         .stream()
+                         .peek(item -> {
+                           Integer countFromOrder = orderItemCounts.get(item.getId());
+                           if (countFromOrder != null) {
+                             item.setCount(countFromOrder);
+                           }
+                         })
+                         .toList();
+  }
+
+  private Map<Long, Integer> getItemsCountFromOrderItems(Long id) {
+    return orderItemRepository.findOrderItemsByOrderId(id)
+                              .stream()
+                              .collect(Collectors.toMap(
+                                  OrderItem::getItemId,
+                                  OrderItem::getCount
+                              ));
+  }
+
   private void flushItemAndCart(Long cartId) {
     Cart cart = new Cart(cartId, TOTAL_INIT, new ArrayList<>());
     cartRepository.save(cart);
@@ -222,5 +178,71 @@ public class ItemServiceImpl implements ItemService {
     cartItemRepository.deleteAllByCartId(cartId);
 
     itemRepository.updateAllCountToZero();
+  }
+
+  private static Pageable getPageableItemsRequest(Sorting sort, int pageNumber, int pageSize) {
+    Pageable pageableItems;
+    switch (sort) {
+      case ALPHA ->
+          pageableItems = PageRequest.of(pageNumber, pageSize, Sort.by("title").ascending());
+      case PRICE ->
+          pageableItems = PageRequest.of(pageNumber, pageSize, Sort.by("price").ascending());
+      case NO -> pageableItems = PageRequest.of(pageNumber, pageSize);
+      default -> throw new SortingException("Некорректный тип сортировки: " + sort);
+    }
+    return pageableItems;
+  }
+
+  private double calculateTotal() {
+    return cartItemRepository.findCartItemsByCartId(CART_ID)
+                             .stream()
+                             .mapToDouble(cartItem -> {
+                               Item product = itemRepository.getReferenceById(
+                                   cartItem.getItemId());
+                               return product.getPrice() * cartItem.getCount();
+                             })
+                             .sum();
+  }
+
+  private void decrementItem(Long itemId, Optional<CartItem> existingCartItem) {
+    if (existingCartItem.isPresent()) {
+      CartItem cartItem = existingCartItem.get();
+
+      if (cartItem.getCount() >= 1) {
+        cartItem.setCount(cartItem.getCount() - 1);
+        cartItemRepository.save(cartItem);
+
+        itemRepository.decrementCount(itemId);
+      } else {
+        cartItemRepository.delete(cartItem);
+      }
+    }
+  }
+
+  private void incrementItem(Long itemId, Optional<CartItem> existingCartItem) {
+    if (existingCartItem.isPresent()) {
+      CartItem cartItem = existingCartItem.get();
+
+      cartItem.setCount(cartItem.getCount() + 1);
+      cartItemRepository.save(cartItem);
+    } else {
+      CartItem newItem = new CartItem();
+      newItem.setCartId(CART_ID);
+      newItem.setItemId(itemId);
+      newItem.setCount(1);
+
+      cartItemRepository.save(newItem);
+    }
+    itemRepository.incrementCount(itemId);
+  }
+
+  private List<Item> getItemsFromCart() {
+    return cartItemRepository.findCartItemsByCartId(CART_ID)
+                             .stream()
+                             .map(CartItem::getItemId)
+                             .collect(Collectors.collectingAndThen(
+                                 Collectors.toList(),
+                                 itemRepository::findAllById
+                             ));
   }
 }
