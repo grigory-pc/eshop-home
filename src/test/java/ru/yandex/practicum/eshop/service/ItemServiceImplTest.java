6b603ebc94009b2257c9ad2d06f0238135cac164
Grigory Trubnikov <grigoriy.trubnikov@ebs.ru>
2025-05-07 03:56:05 +0300

    feat: UT

--- src/test/java/ru/yandex/practicum/eshop/service/ItemServiceImplTest.java
+++ src/test/java/ru/yandex/practicum/eshop/service/ItemServiceImplTest.java
@@ -1,41 +1,139 @@
 package ru.yandex.practicum.eshop.service;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.yandex.practicum.eshop.dto.ItemDto;
+import ru.yandex.practicum.eshop.entity.Cart;
+import ru.yandex.practicum.eshop.entity.CartItem;
+import ru.yandex.practicum.eshop.entity.Item;
+import ru.yandex.practicum.eshop.enums.Sorting;
+import ru.yandex.practicum.eshop.mappers.ItemMapper;
+import ru.yandex.practicum.eshop.repository.*;
+import ru.yandex.practicum.eshop.utils.Data;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ItemServiceImpl.class)
 class ItemServiceImplTest {
+    public static final String MESSAGE_FAIL = "Не ожидали получить исключение";
+    private static final Long ITEM_ID = 1L;
+    private static final Long CART_ID = 1L;
+    private static final Long ORDER_ID = 1L;
 
-  @BeforeEach
-  void setUp() {
-  }
+    @MockBean
+    private ItemMapper itemMapper;
+    @MockBean
+    private ItemRepository itemRepository;
+    @MockBean
+    private CartRepository cartRepository;
+    @MockBean
+    private OrderRepository orderRepository;
+    @MockBean
+    private CartItemRepository cartItemRepository;
+    @MockBean
+    private OrderItemRepository orderItemRepository;
+    @Autowired
+    private ItemServiceImpl itemService;
 
-  @Test
-  void getItems() {
-  }
+    @Test
+    @DisplayName("Позитивный тест - проверяем получение списка товаров")
+    void positiveTest_shouldGetItems() {
+        try {
+            String search = "";
+            Sorting sort = Sorting.ALPHA;
+            int pageNumber = 0;
+            int pageSize = 10;
 
-  @Test
-  void editCart() {
-  }
+            Page<Item> itemPage = new PageImpl<>(List.of(Data.getItemPlusCount()));
+            ItemDto expectedItemDto = Data.getItemDto();
 
-  @Test
-  void getCartItems() {
-  }
+            doReturn(itemPage)
+                    .when(itemRepository).findAll(any(Pageable.class));
+            doReturn(new PageImpl<>(List.of(expectedItemDto)))
+                    .when(itemMapper).toDtoPage(any());
 
-  @Test
-  void getItem() {
-  }
+            Page<ItemDto> actualItemDto = itemService.getItems(search, sort, pageNumber, pageSize);
 
-  @Test
-  void buyItems() {
-  }
+            assertEquals(1, actualItemDto.getTotalElements());
+            assertEquals(expectedItemDto, actualItemDto.getContent().get(0));
 
-  @Test
-  void getOrders() {
-  }
+        } catch (Exception e) {
+            fail(MESSAGE_FAIL);
+        }
+    }
 
-  @Test
-  void getOrderItems() {
-  }
+    @Test
+    @DisplayName("Позитивный тест - проверяем изменение(увеличение) количества товаров в корзине")
+    void positiveTest_shouldEditCart() {
+        try {
+            String action = "plus";
+            Cart cart = Data.getCart();
+            CartItem cartItem = Data.getCartItem();
+
+            doReturn(cart)
+                    .when(cartRepository).getReferenceById(anyLong());
+            doReturn(Optional.of(cartItem))
+                    .when(cartItemRepository).findCartItemByCartIdAndItemId(anyLong(), anyLong());
+            doReturn(cartItem)
+                    .when(cartItemRepository).save(any(CartItem.class));
+            doNothing().when(itemRepository)
+                    .incrementCount(anyLong());
+            doReturn(cart)
+                    .when(cartRepository).save(any(Cart.class));
+
+            itemService.editCart(ITEM_ID, action);
+
+            verify(cartRepository, atLeastOnce()).getReferenceById(anyLong());
+            verify(cartItemRepository, atLeastOnce()).findCartItemByCartIdAndItemId(anyLong(), anyLong());
+            verify(cartItemRepository, atLeastOnce()).save(any(CartItem.class));
+            verify(itemRepository, atLeastOnce()).incrementCount(anyLong());
+            verify(cartRepository, atLeastOnce()).save(any(Cart.class));
+
+        } catch (Exception e) {
+            fail(MESSAGE_FAIL);
+        }
+    }
+
+    @Test
+    @DisplayName("Позитивный тест - проверяем получение корзины с товарами")
+    void positiveTest_shouldGetCartItems() {
+    }
+
+    @Test
+    @DisplayName("Позитивный тест - проверяем получение данных товара")
+    void positiveTest_shouldGetItem() {
+    }
+
+    @Test
+    @DisplayName("Позитивный тест - проверяем формирование заказа из корзины с товарами")
+    void positiveTest_shouldBuyItems() {
+    }
+
+    @Test
+    @DisplayName("Позитивный тест - проверяем получение списка заказов")
+    void positiveTest_shouldGetOrders() {
+    }
+
+    @Test
+    @DisplayName("Позитивный тест - проверяем получение данных заказа")
+    void positiveTest_shouldGetOrderItems() {
+    }
 }
--- src/test/java/ru/yandex/practicum/eshop/utils/Data.java
+++ src/test/java/ru/yandex/practicum/eshop/utils/Data.java
@@ -1,52 +1,70 @@
 package ru.yandex.practicum.eshop.utils;
 
 import lombok.experimental.UtilityClass;
-import ru.yandex.practicum.eshop.entity.CartItem;
-import ru.yandex.practicum.eshop.entity.Item;
-import ru.yandex.practicum.eshop.entity.Order;
-import ru.yandex.practicum.eshop.entity.OrderItem;
+import ru.yandex.practicum.eshop.dto.ItemDto;
+import ru.yandex.practicum.eshop.entity.*;
 
 @UtilityClass
 public class Data {
-  private static final Long ITEM_ID = 1L;
-  private static final Long CART_ID = 1L;
-  private static final Long ORDER_ID = 1L;
-  private static final String ITEM_SHORTS_IMG_PATH = "/images/shorts.jpg";
+    private static final Long ITEM_ID = 1L;
+    private static final Long CART_ID = 1L;
+    private static final Long ORDER_ID = 1L;
+    private static final String ITEM_SHORTS_IMG_PATH = "/images/shorts.jpg";
 
-  public Item getItemPlusCount() {
-    return Item.builder()
-               .id(ITEM_ID)
-               .title("Шорты")
-               .imgPath(ITEM_SHORTS_IMG_PATH)
-               .description("Летние шорты карго")
-               .price(1399.99)
-               .count(1)
-               .build();
-  }
+    public Item getItemPlusCount() {
+        return Item.builder()
+                .id(ITEM_ID)
+                .title("Шорты")
+                .imgPath(ITEM_SHORTS_IMG_PATH)
+                .description("Летние шорты карго")
+                .price(1399.99)
+                .count(1)
+                .build();
+    }
 
-  public Order getOrder() {
-    Item item = getItemPlusCount();
+    public ItemDto getItemDto() {
+        return ItemDto.builder()
+                .id(ITEM_ID)
+                .title("Шорты")
+                .imgPath(ITEM_SHORTS_IMG_PATH)
+                .description("Летние шорты карго")
+                .price(1399.99)
+                .count(1)
+                .build();
+    }
 
-    return Order.builder()
+    public Order getOrder() {
+        Item item = getItemPlusCount();
+
+        return Order.builder()
                 .id(ORDER_ID)
                 .totalSum(item.getPrice())
                 .build();
-  }
+    }
 
-  public CartItem getCartItem() {
-    return CartItem.builder()
-                   .cartId(CART_ID)
-                   .itemId(ITEM_ID)
-                   .count(1)
-                   .build();
+    public Cart getCart() {
+        Item item = getItemPlusCount();
 
-  }
+        return Cart.builder()
+                .id(CART_ID)
+                .total(item.getPrice())
+                .build();
+    }
 
-  public OrderItem getOrderItem() {
-    return OrderItem.builder()
-                    .orderId(ORDER_ID)
-                    .itemId(ITEM_ID)
-                    .count(1)
-                    .build();
-  }
+    public CartItem getCartItem() {
+        return CartItem.builder()
+                .cartId(CART_ID)
+                .itemId(ITEM_ID)
+                .count(1)
+                .build();
+
+    }
+
+    public OrderItem getOrderItem() {
+        return OrderItem.builder()
+                .orderId(ORDER_ID)
+                .itemId(ITEM_ID)
+                .count(1)
+                .build();
+    }
 }
